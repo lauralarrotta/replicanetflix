a78f1df4c9a519dbee817eaa20028200a59b5648
lauralarrotta <larrotta.laura@gmail.com>
2024-05-15 06:13:58 -0500

    express II

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,15 +1,9 @@
 const renderCards = require("./renderCards");
-
 const axios = require("axios");
-
-// // Utiliza jQuery para hacer la solicitud AJAX y manipular el DOM
-// $.get("https://students-api.up.railway.app/movies", function (data, status) {
-//   renderCards(data);
-// });
 
 //Solicitud GET a la URL proporcionada, que es la API de películas.
 axios
-  .get("https://students-api.up.railway.app/movies")
+  .get("http://localhost:3000/movies")
   .then((response) => {
     const data = response.data;
     renderCards(data);
